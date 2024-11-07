bcaf5d26f0bd02983de9a4468f5fad1af28bef56
msk226 <kminseok5167@gmail.com>
2024-11-07 21:08:48 +0900

    feat(model): Promotion 검증 로직 추가

--- src/main/java/store/model/Promotion.java
+++ src/main/java/store/model/Promotion.java
@@ -1,5 +1,10 @@
 package store.model;
 
+
+import static store.utils.ErrorMessage.INVALID_PROMOTION_DATE;
+import static store.utils.ErrorMessage.PROMOTION_BUY_AMOUNT_GREATER_THAN_ZERO;
+import static store.utils.ErrorMessage.PROMOTION_GET_AMOUNT_GREATER_THAN_ZERO;
+
 import java.time.LocalDate;
 
 public class Promotion {
@@ -11,6 +16,7 @@ public class Promotion {
     private final LocalDate endDate;
 
     public Promotion(String name, Integer buyAmount, Integer getAmount, LocalDate startDate, LocalDate endDate) {
+        validatePromotion(buyAmount, getAmount, startDate, endDate);
         this.name = name;
         this.buyAmount = buyAmount;
         this.getAmount = getAmount;
@@ -18,6 +24,29 @@ public class Promotion {
         this.endDate = endDate;
     }
 
+    private void validatePromotion(Integer buyAmount, Integer getAmount, LocalDate startDate, LocalDate endDate){
+        validatePromotionBuy(buyAmount);
+        validatePromotionGet(getAmount);
+        validatePromotionDate(startDate, endDate);
+    }
+
+    private void validatePromotionBuy(Integer buyAmount){
+        if (buyAmount < 0) {
+            throw new IllegalArgumentException(PROMOTION_BUY_AMOUNT_GREATER_THAN_ZERO);
+        }
+    }
+
+    private void validatePromotionGet(Integer getAmount){
+        if (getAmount < 0) {
+            throw new IllegalArgumentException(PROMOTION_GET_AMOUNT_GREATER_THAN_ZERO);
+        }
+    }
+
+    private void validatePromotionDate(LocalDate startDate, LocalDate endDate){
+        if (startDate.isAfter(endDate)) {
+            throw new IllegalArgumentException(INVALID_PROMOTION_DATE);
+        }
+    }
     public String getName() {
         return name;
     }
---
+++ src/main/java/store/utils/ErrorMessage.java
@@ -0,0 +1,12 @@
+package store.utils;
+
+public class ErrorMessage {
+
+
+    public static final String PROMOTION_BUY_AMOUNT_GREATER_THAN_ZERO = "[ERROR] 프로모션의 구매 수량은 항상 0보다 커야 합니다.";
+    public static final String PROMOTION_GET_AMOUNT_GREATER_THAN_ZERO = "[ERROR] 프로모션의 증정 수량은 항상 0보다 커야 합니다.";
+    public static final String INVALID_PROMOTION_DATE = "[ERROR] 프로모션의 시작 날짜는 종료 날짜 이전이어야 한다.";
+
+
+
+}
