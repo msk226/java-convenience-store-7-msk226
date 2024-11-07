9a74f4f2b62abf158d17000a7bb884692f47d303
msk226 <kminseok5167@gmail.com>
2024-11-07 21:01:00 +0900

    feat(model): 상품 정보를 저장할 Product 객체 생성

---
+++ src/main/java/store/model/Product.java
@@ -0,0 +1,20 @@
+package store.model;
+
+public class Product {
+    private final String name;
+    private final Integer price;
+    private final Promotion promotion;
+
+    public Product(String name, Integer price, Promotion promotion){
+        validatePrice(price);
+        this.name = name;
+        this.price = price;
+        this.promotion = promotion;
+    }
+
+    private void validatePrice(Integer price){
+        if (price <= 0){
+            throw new IllegalArgumentException("[ERROR] 상품의 가격은 항상 0보다 커야 합니다.");
+        }
+    }
+}
