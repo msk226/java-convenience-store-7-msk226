99fc344b5dde357ada6df23abd3eb3d1663b9980
msk226 <kminseok5167@gmail.com>
2024-11-07 21:03:03 +0900

    test(model): 프로모션 정보를 저장할 Promotion에 대한 테스트 코드 작성

---
+++ src/test/java/store/model/PromotionTest.java
@@ -0,0 +1,54 @@
+package store.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import java.time.LocalDate;
+import org.junit.jupiter.api.Test;
+
+public class PromotionTest extends NsTest {
+
+    private final String NAME = "MD추천상품";
+    private final Integer BUY_AMOUNT = 1;
+    private final Integer GET_AMOUNT = 1;
+    private final LocalDate START_DATE = LocalDate.of(2024,1,1);
+    private final LocalDate END_DATE = LocalDate.of(2024,12,31);
+
+
+    @Test
+    void 프로모션은_필수_속성을_가져야_한다(){
+        Promotion promotion = new Promotion(NAME, BUY_AMOUNT, GET_AMOUNT, START_DATE, END_DATE);
+
+        assertEquals(promotion.getName(), NAME);
+        assertEquals(promotion.getBuyAmount(), BUY_AMOUNT);
+        assertEquals(promotion.getGetAmount(), GET_AMOUNT);
+        assertEquals(promotion.getStartDate(), START_DATE);
+        assertEquals(promotion.getEndDate(), END_DATE);
+    }
+
+    @Test
+    void 프로모션의_구매수량은_항상_0보다_크거나_같아야한다(){
+        assertThrows(IllegalArgumentException.class,
+                () -> new Promotion(NAME, -10, GET_AMOUNT, START_DATE, END_DATE));
+    }
+    @Test
+    void 프로모션의_증정수량은_항상_0보다_크거나_같아야한다() {
+        assertThrows(IllegalArgumentException.class,
+                () -> new Promotion(NAME, BUY_AMOUNT, -10, START_DATE, END_DATE));
+    }
+    @Test
+    void 프로모션의_시작날짜는_종료날짜_이전이어야_한다(){
+        assertThrows(IllegalArgumentException.class,
+                () -> new Promotion(NAME, BUY_AMOUNT, GET_AMOUNT, LocalDate.MAX, END_DATE));
+    }
+    @Test
+    void 프로모션의_종료날짜는_시작날짜_이후여야_한다(){
+        assertThrows(IllegalArgumentException.class,
+                () -> new Promotion(NAME, BUY_AMOUNT, GET_AMOUNT, START_DATE, LocalDate.MIN));
+    }
+
+    @Override
+    protected void runMain() {
+
+    }
+}
