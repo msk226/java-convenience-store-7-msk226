a929d08179fecdbf17c725445a35d97006223257
msk226 <kminseok5167@gmail.com>
2024-11-08 19:43:13 +0900

    feat(controller): 프로모션 값을 입력 받아 이를 객체로 변환 할 컨버터 생성

---
+++ src/main/java/store/converter/PromotionConverter.java
@@ -0,0 +1,89 @@
+package store.converter;
+
+import static store.utils.constant.PromotionConstant.BUY_AMOUNT_INDEX;
+import static store.utils.constant.PromotionConstant.END_DATE_INDEX;
+import static store.utils.constant.PromotionConstant.GET_AMOUNT_INDEX;
+import static store.utils.constant.PromotionConstant.NAME_INDEX;
+import static store.utils.constant.PromotionConstant.PROMOTION_DATE_FORMAT;
+import static store.utils.constant.PromotionConstant.PROMOTION_INPUT_LENGTH;
+import static store.utils.constant.PromotionConstant.PROMOTION_SPILT_REGEX;
+import static store.utils.constant.PromotionConstant.PROMOTION_TITLE_INDEX;
+import static store.utils.constant.PromotionConstant.START_DATE_INDEX;
+import static store.utils.message.ErrorMessage.*;
+import static store.utils.message.ErrorMessage.INVALID_INPUT;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.ArrayList;
+import java.util.List;
+import store.model.Promotion;
+import store.utils.message.ErrorMessage;
+
+public class PromotionConverter {
+    public List<Promotion> convertToPromotion(List<String> inputPromotions){
+        List<Promotion> promotions = new ArrayList<>();
+
+        for (String inputPromotion : inputPromotions.subList(PROMOTION_TITLE_INDEX, inputPromotions.size())){
+            String[] input = parseInputPromotion(inputPromotion);
+            Promotion promotion = createPromotion(input);
+            promotions.add(promotion);
+        }
+        return promotions;
+    }
+
+
+    private Promotion createPromotion(String[] input) {
+        String name = parseName(input);
+        Integer buyAmount = parseBuyAmount(input);
+        Integer getAmount = parseGetAmount(input);
+        LocalDate startDate = parseStartDate(input);
+        LocalDate endDate = parseEndDate(input);
+        return new Promotion(name, buyAmount, getAmount, startDate, endDate);
+    }
+
+    private String parseName(String[] input) {
+        return input[NAME_INDEX];
+    }
+
+    private Integer parseBuyAmount(String[] input) {
+        try {
+            return Integer.valueOf(input[BUY_AMOUNT_INDEX]);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Invalid buy amount");
+        }
+    }
+
+    private Integer parseGetAmount(String[] input) {
+        try {
+            return Integer.valueOf(input[GET_AMOUNT_INDEX]);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Invalid get amount");
+        }
+    }
+
+    private LocalDate parseStartDate(String[] input) {
+        try {
+            return LocalDate.parse(input[START_DATE_INDEX], DateTimeFormatter.ofPattern(PROMOTION_DATE_FORMAT));
+        } catch (DateTimeParseException e) {
+            throw new IllegalArgumentException("Invalid start date format");
+        }
+    }
+
+    private LocalDate parseEndDate(String[] input) {
+        try {
+            return LocalDate.parse(input[END_DATE_INDEX], DateTimeFormatter.ofPattern(PROMOTION_DATE_FORMAT));
+        } catch (DateTimeParseException e) {
+            throw new IllegalArgumentException("Invalid end date format");
+        }
+    }
+
+
+    private String[] parseInputPromotion(String inputPromotion) {
+        String[] input = inputPromotion.split(PROMOTION_SPILT_REGEX);
+        if (input.length != PROMOTION_INPUT_LENGTH) {
+            throw new IllegalArgumentException(INVALID_INPUT);
+        }
+        return input;
+    }
+}
---
+++ src/main/java/store/utils/constant/PromotionConstant.java
@@ -0,0 +1,29 @@
+package store.utils.constant;
+
+import java.time.LocalDate;
+
+public class PromotionConstant {
+
+
+    public static final Integer PROMOTION_TITLE_INDEX = 1;
+
+    public static final Integer NAME_INDEX = 0;
+    public static final Integer BUY_AMOUNT_INDEX = 1;
+    public static final Integer GET_AMOUNT_INDEX = 2;
+    public static final Integer START_DATE_INDEX = 3;
+    public static final Integer END_DATE_INDEX = 4;
+
+    public static final Integer PROMOTION_INPUT_LENGTH = 5;
+
+    public static final String PROMOTION_SPILT_REGEX = ",";
+    public static final String PROMOTION_DATE_FORMAT = "yyyy-MM-dd";
+
+
+    public static final String DEFAULT_NAME = "null";
+    public static final Integer DEFAULT_BUY_AMOUNT = 0;
+    public static final Integer DEFAULT_GET_AMOUNT = 0;
+    public static final LocalDate DEFAULT_START_DATE = LocalDate.MIN;
+    public static final LocalDate DEFAULT_END_DATE = LocalDate.MAX;
+
+
+}
