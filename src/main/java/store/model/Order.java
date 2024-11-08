bd37eab44afcab5789be1f682c23886f433c6f9c
msk226 <kminseok5167@gmail.com>
2024-11-08 14:06:55 +0900

    feat(model): 결제 및 할인 금액 관련 메서드 구현

--- src/main/java/store/model/Order.java
+++ src/main/java/store/model/Order.java
@@ -2,17 +2,31 @@ package store.model;
 
 import static store.utils.message.ErrorMessage.ORDER_QUANTITY_GREATER_THAN_ZERO;
 
+import java.time.LocalDate;
+
 public class Order {
 
     private final Product product;
     private final Integer quantity;
+    private final LocalDate orderDate;
 
     public Order(Product product, Integer quantity){
         validateOrder(quantity);
         this.product = product;
         this.quantity = quantity;
+        this.orderDate = LocalDate.now();
     }
 
+    public int getTotalAmount() {
+        return product.getPrice() * quantity;
+    }
+    public int getDiscountAmount(){
+        Promotion promotion = product.getPromotion();
+        if (promotion == null){
+            return 0;
+        }
+        return promotion.calculateDiscount(quantity, product.getPrice(), orderDate);
+    }
     public Product getProduct() {
         return product;
     }
@@ -31,4 +45,5 @@ public class Order {
             throw new IllegalArgumentException(ORDER_QUANTITY_GREATER_THAN_ZERO);
         }
     }
+
 }
--- src/main/java/store/model/Store.java
+++ src/main/java/store/model/Store.java
@@ -12,6 +12,26 @@ public class Store {
         this.inventory = inventory;
     }
 
+    public int calculateTotalAmount(List<Order> orders) {
+        int totalAmount = 0;
+        for (Order order : orders){
+            totalAmount += order.getTotalAmount();
+        }
+        return totalAmount;
+    }
+
+    public int calculateDiscountAmount(List<Order> orders) {
+        int totalDiscountAmount = 0;
+        for (Order order : orders){
+            totalDiscountAmount += order.getDiscountAmount();
+        }
+        return totalDiscountAmount;
+    }
+
+    public int calculateFinalAmount(List<Order> orders) {
+        return calculateTotalAmount(orders) - calculateDiscountAmount(orders);
+    }
+
     public List<Order> processOrder(List<Order> orders){
         List<Order> successOrders = new ArrayList<>();
         for (Order order : orders){
