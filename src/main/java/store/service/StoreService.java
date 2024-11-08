f738ddb25c5814f70a116f553d0e72e4dd71d1cb
msk226 <kminseok5167@gmail.com>
2024-11-08 22:16:43 +0900

    feat(service): 프로모션 적용이 가능한 상품에 대해 고객이 해당 수량만큼 가져온지 검증 할 서비스 메서드 작성

--- src/main/java/store/service/StoreService.java
+++ src/main/java/store/service/StoreService.java
@@ -1,11 +1,14 @@
 package store.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import store.model.Inventory;
 import store.model.Order;
 import store.model.Product;
 import store.model.Store;
+import store.view.InputView;
 
 public class StoreService {
 
@@ -19,4 +22,21 @@ public class StoreService {
         return store.processOrder(orders);
     }
 
+    //TODO 프로모션 적용이 가능한 상품에 대해 고객이 해당 수량만큼 가져오지 않았을 경우, 혜택에 대한 안내 메시지를 출력한다. -> 검증 로직 만들어짐
+    //TODO 프로모션 재고가 부족하여 일부 수량을 프로모션 혜택 없이 결제해야 하는 경우, 일부 수량에 대해 정가로 결제할지 여부에 대한 안내 메시지를 출력한다.-> 얘도 뭐 대충은 된 듯
+    //TODO 멤버십 할인 적용 여부를 확인하기 위해 안내 문구를 출력한다.
+    //TODO 구매 상품 내역, 증정 상품 내역, 금액 정보를 출력한다. -> 얘도 거의 다 된 듯
+    //TODO 추가 구매 여부를 확인하기 위해 안내 문구를 출력한다. -> 얘도 금방 됨
+
+    public List<Product> checkEligibleFreeItems(Map<Product, Integer> orderResults, Store store){
+        List<Product> promotionProduct = new ArrayList<>();
+
+        Set<Product> products = orderResults.keySet();
+        for (Product product : products){
+            if (store.checkEligibleFreeItems(product, orderResults.get(product))){
+                promotionProduct.add(product);
+            }
+        }
+        return promotionProduct;
+    }
 }
