0150777214b5a28e8ffb08dc5673c8c8c55c5b59
msk226 <kminseok5167@gmail.com>
2024-11-08 22:16:13 +0900

    feat(utils): 프로모션 추가 혜택 메시지 추가

--- src/main/java/store/utils/message/InputMessage.java
+++ src/main/java/store/utils/message/InputMessage.java
@@ -2,4 +2,6 @@ package store.utils.message;
 
 public class InputMessage {
     public static final String ORDER_MESSAGE = "구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])\n";
+    public static final String PROMOTION_MESSAGE_TEMPLATE = "%s은(는) %d개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)";
+
 }
