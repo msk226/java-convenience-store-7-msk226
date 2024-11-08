5c7ba84448be740fea44590cb80c69a0ec70ac0f
msk226 <kminseok5167@gmail.com>
2024-11-08 19:55:34 +0900

    feat(controller): 컨버터에서 Product Map으로 변환 하도록 메서드 작성

--- src/main/java/store/controller/StoreController.java
+++ src/main/java/store/controller/StoreController.java
@@ -1,6 +1,17 @@
 package store.controller;
 
+import static store.utils.constant.ProductConstant.PRODUCT_INPUT_LENGTH;
+import static store.utils.constant.ProductConstant.PRODUCT_SPILT_REGEX;
+import static store.utils.constant.ProductConstant.QUANTITY_INDEX;
+import static store.utils.message.ErrorMessage.INVALID_INPUT;
+
 import java.util.List;
+import java.util.Map;
+import store.converter.ProductConverter;
+import store.converter.PromotionConverter;
+import store.model.Inventory;
+import store.model.Product;
+import store.model.Promotion;
 import store.view.InputView;
 
 public class StoreController {
@@ -9,7 +20,9 @@ public class StoreController {
         List<String> inputPromotions = InputView.inputData("src/main/resources/promotions.md");
         List<String> inputProducts = InputView.inputData("src/main/resources/products.md");
 
-
+        List<Promotion> promotions = PromotionConverter.convertToPromotion(inputPromotions);
+        Map<Product, Integer> products = ProductConverter.convertToProduct(inputProducts, promotions);
 
     }
+
 }
--- src/main/java/store/converter/ProductConverter.java
+++ src/main/java/store/converter/ProductConverter.java
@@ -3,18 +3,46 @@ package store.converter;
 import static store.utils.constant.ProductConstant.NAME_INDEX;
 import static store.utils.constant.ProductConstant.PRICE_INDEX;
 import static store.utils.constant.ProductConstant.PRODUCT_INPUT_LENGTH;
+import static store.utils.constant.ProductConstant.PRODUCT_SPILT_REGEX;
 import static store.utils.constant.ProductConstant.PROMOTION_INDEX;
+import static store.utils.constant.ProductConstant.QUANTITY_INDEX;
+import static store.utils.constant.PromotionConstant.PROMOTION_TITLE_INDEX;
 import static store.utils.message.ErrorMessage.INVALID_INPUT;
 import static store.utils.message.ErrorMessage.INVALID_PROMOTION_NAME;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import store.model.Product;
 import store.model.Promotion;
 import store.utils.message.ErrorMessage;
 
 public class ProductConverter {
 
-    public static Product createProduct(String[] input, List<Promotion> promotions) {
+    public static Map<Product, Integer> convertToProduct(List<String> inputProducts, List<Promotion> promotions){
+        Map<Product, Integer> products = new HashMap<>();
+        for (String inputProduct : inputProducts.subList(PROMOTION_TITLE_INDEX, inputProducts.size())){
+            String[] input = parseInputProduct(inputProduct);
+            Product product = createProduct(input, promotions);
+            products.put(product, getQuantity(input));
+        }
+        return products;
+    }
+
+    private static int getQuantity(String[] input) {
+        return Integer.parseInt(input[QUANTITY_INDEX]);
+    }
+
+    private static String[] parseInputProduct(String inputProduct) {
+        String[] input = inputProduct.split(PRODUCT_SPILT_REGEX);
+        if (input.length != PRODUCT_INPUT_LENGTH) {
+            throw new IllegalArgumentException(INVALID_INPUT);
+        }
+        return input;
+    }
+
+    private static Product createProduct(String[] input, List<Promotion> promotions) {
         validateProductInput(input);
         String name = getProductName(input);
         Integer price = parsePrice(input);
