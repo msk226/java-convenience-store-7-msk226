b457003f3fb914092501e3063864eab9f0e3c305
msk226 <kminseok5167@gmail.com>
2024-11-09 10:29:16 +0900

    feat(service): 하드코딩 된 숫자들 상수화

--- src/main/java/store/service/StoreService.java
+++ src/main/java/store/service/StoreService.java
@@ -11,6 +11,7 @@ import store.model.Store;
 import store.view.InputView;
 
 public class StoreService {
+    private static final Integer FREE_ITEM = 1;
 
     public Store initializeStore(Map<Product, Integer> products){
         Inventory inventory = new Inventory();
@@ -22,7 +23,6 @@ public class StoreService {
         return store.processOrder(orders);
     }
 
-    //TODO 프로모션 재고가 부족하여 일부 수량을 프로모션 혜택 없이 결제해야 하는 경우, 일부 수량에 대해 정가로 결제할지 여부에 대한 안내 메시지를 출력한다.-> 얘도 뭐 대충은 된 듯
     //TODO 멤버십 할인 적용 여부를 확인하기 위해 안내 문구를 출력한다.
     //TODO 구매 상품 내역, 증정 상품 내역, 금액 정보를 출력한다. -> 얘도 거의 다 된 듯
     //TODO 추가 구매 여부를 확인하기 위해 안내 문구를 출력한다. -> 얘도 금방 됨
@@ -43,6 +43,6 @@ public class StoreService {
 
     public Map<Product, Integer> getFreeItem(Product product, Store store){
         Map<Product, Integer> orderResults = store.getOrderResult();
-        return store.getFreeItem(orderResults, product, 1);
+        return store.getFreeItem(orderResults, product, FREE_ITEM);
     }
 }
